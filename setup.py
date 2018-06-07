d517be7a5fb15918ea0a36e274cd4bd1e8fd2f5c
Fabian Senf <senf@tropos.de>
2018-06-07 09:39:14 +0000

    started python scripting possibility

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(name='coorga',
       version = __version__,
-      description='Collection of Convective Organization Tools',
-      author='Fabian Senf',
-      author_email='senf@tropos.de',
-      license='GPL',
-      packages=find_packages(),
+      description = 'Collection of Convective Organization Tools',
+      author = 'Fabian Senf',
+      author_email = 'senf@tropos.de',
+      license = 'GPL',
+      packages = find_packages(),
 #['coorga', 'coorga.inout', 'coorga.object_creation', 'coorga.metrics'],
-      zip_safe=False)
+      zip_safe = False)
 
