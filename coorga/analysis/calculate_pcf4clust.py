corrected pcf calculation: time-dependent cell number and option for homogeneous cell densities.

coorga/analysis/calculate_pcf4clust.py
@@ -15,6 +15,7 @@ from coorga.metrics.pair_correlation_function import radius_ring_edges, pairCorr
 def calculate_pcf4clust( c, egrid, nd_ref, 
                          weight = 1., 
                          rMax = 500., 
+                         pcf_sensitivity = True,
                          dr = 20.,
                          verbose = False):
 
@@ -60,8 +61,21 @@ def calculate_pcf4clust( c, egrid, nd_ref,
     ncells = len( c['abs_time'] )
     # ================================================================
 
+    
+    # get norm of cell density function ------------------------------
+    dx = gi.lmean(np.diff(egrid[0], axis = 0), axis = 1)
+    dy = gi.lmean(np.diff(egrid[1], axis = 1), axis = 0)
 
+    ncells_per_slot = (dx * dy * nd).sum()
+    nd_normed = nd_ref / ncells_per_slot
+    # ================================================================
 
+    
+    # and also define homogeneous cell density function --------------
+    nd_const = 1. / (dx * dy * np.size(nd_ref))
+    # ================================================================
+    
+    
     # get cell coordinates and cutout region -------------------------
     xt, yt = c['x_mean'], c['y_mean']
     # ================================================================
@@ -78,8 +92,7 @@ def calculate_pcf4clust( c, egrid, nd_ref,
     rbins = radius_ring_edges(rMax, dr, weight = weight)
     nbins = len(rbins)
 
-    
-    
+   
     c['pcf'] = np.nan * np.ma.ones( (ncells, nbins - 1) )
     
 
@@ -88,22 +101,46 @@ def calculate_pcf4clust( c, egrid, nd_ref,
         # masking 
         m = (c[tname] == tid) 
         ccount = m.sum()
-        # ccount_inside_region = (m & rmask).sum()
+        
 
         pos = np.array([xt[m], yt[m]]).T
 
         if verbose:
             print tid, m.sum()
 
+        # correct for temporal variation of cell number
+        nd_corr = ccount * nd_normed
+        
         # pair-correlation analysis with variable BG density
-        gfull, g, rbins, indices = pairCorrelationFunction_2D(pos, egrid, nd_ref, rMax, dr, weight = weight)
+        gfull, g, rbins, indices = pairCorrelationFunction_2D(pos, egrid, nd_corr, rMax, dr, weight = weight)
 
         gfull_with_edge = np.nan *  np.ma.ones( (ccount, nbins - 1) )
         gfull_with_edge[indices] = gfull
         c['pcf'][m] = gfull_with_edge
+        c['pcf'] = np.ma.masked_invalid( c['pcf'] )
+        
+        if pcf_sensitivity:
+            
+            nd = {}
+            nd['pcf_hom'] = ccount * nd_const
+            nd['pcf_hom_tconst'] = ncells_per_slot * nd_const
+            nd['pcf_tconst'] = ncells_per_slot * nd_normed
+            
+            for pcf_name in nd.keys():
+                
+                # pair-correlation analysis with variable BG density
+                gfull, g, rbins, indices = pairCorrelationFunction_2D(pos, 
+                                                                      egrid, 
+                                                                      nd[pcf_name],
+                                                                      rMax, dr, weight = weight)
+
+                gfull_with_edge = np.nan *  np.ma.ones( (ccount, nbins - 1) )
+                gfull_with_edge[indices] = gfull
+                
+                c[pcf_name][m] = gfull_with_edge
+                c[pcf_name] = np.ma.masked_invalid( c[pcf_name] )
 
 
-    c['pcf'] = np.ma.masked_invalid( c['pcf'] )
     c['rbins_pcf'] = rbins
     # ================================================================
 